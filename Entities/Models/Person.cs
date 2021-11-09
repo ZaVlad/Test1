1e1aca24dc67168e5ee9caf16041802c59a2d0e2
Vlad Zaremskyi <zaremskyivlad@gmail.com>
2021-11-09 14:57:06 +0200

    First commit

---
+++ Entities/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Infrastructure.Interfaces/DataAccess/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Interfaces.DataAccess
+{
+    interface IDbContext 
+    {
+        public DbSet<Person> Person { get; set; }
+    }
+}
